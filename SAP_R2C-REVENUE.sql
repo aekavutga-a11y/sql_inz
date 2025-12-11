b6b94040d6559a1417345b88001b0c977d4dd792
aekavutga-a11y <aekavut.ga@plobconsult.com>
2025-12-11 08:32:08 +0700

    SAP_R2C-REVENUE-V3.0
    Improve database query speed

--- SAP_R2C-REVENUE.sql
+++ SAP_R2C-REVENUE.sql
@@ -1,6 +1,6 @@
 USE [PLOB]
 GO
-/******* Object:  StoredProcedure [dbo].[pGetIntefaceRevenueHeader_initial]    Script Date: 10/12/2568 14:40:34 ******/
+/***** Object:  StoredProcedure [dbo].[pGetIntefaceRevenueHeader_initial]    Script Date: 10/12/2568 14:40:34 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
